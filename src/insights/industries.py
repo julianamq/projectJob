ad4bda70acdffbb4319cfa495a89ae2efa1ff7ea
Juliana <julianamq20@gmail.com>
2023-03-27 15:51:45 -0300

    ajustes - ajuda Renan Fernandes

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -4,7 +4,7 @@ from src.insights.jobs import read
 
 def get_unique_industries(path: str) -> List[str]:
     """Checks all different industries and returns a list of them"""
-    return set(job["industry"] for job in read(path))
+    return list(set(job["industry"] for job in read(path) if job["industry"]))
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:

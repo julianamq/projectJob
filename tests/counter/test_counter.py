8e2176089b4293e5820ba6dff9c9cafbe720548d
Juliana <julianamq20@gmail.com>
2023-03-27 18:21:43 -0300

    feat: test counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,9 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    assert count_ocurrences("data/jobs.csv", "Python") == 1639
+
+
+# pega o arquivo e procura qual palavra vai ser encontrada.
+# laura dias me ajudou a entender e executar o teste corretamente

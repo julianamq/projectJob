a5c519554b758fdf68f89020fd15c940f8bcd3b4
Juliana <julianamq20@gmail.com>
2023-03-27 17:10:23 -0300

    correção: erros matches_salary, onde tratei o erro com try e expect

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -64,18 +64,13 @@ def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
 def filter_by_salary_range(
     jobs: List[dict], salary: Union[str, int]
 ) -> List[Dict]:
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    raise NotImplementedError
+    # return [job for job in jobs if matches_salary_range(job, salary)]
+    # #isso gera erro.
+    get_filter_by_salary = []
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                get_filter_by_salary.append(job)
+        except ValueError:
+            pass
+    return get_filter_by_salary
